48a4dacf30f7706f3825230e0c96bb6b0e302335
Navin <navin007.a@gmail.com>
2019-12-21 08:14:51 +0530

    Automatic Commit

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ new Vue({
 	el: '#app',
 	data: {
 		title: 'Start Typing!!',
-		link: 'https://www.google.com/'
+		link: 'https://www.google.com/',
+		friendlyLink: '<a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google</a>'
 	},
 	methods: {
 		changeTitle: function(event) {
