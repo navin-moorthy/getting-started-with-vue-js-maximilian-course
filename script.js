818981a9bfd83ac8fbb45ebeb25be142ce144ea1
Navin <navin007.a@gmail.com>
2019-12-21 08:28:00 +0530

    Using VueJS to Interact with the DOM

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ new Vue({
 	data: {
 		title: 'Start Typing!!',
 		link: 'https://www.google.com/',
-		friendlyLink: '<a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google</a>'
+		friendlyLink: '<a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">Google</a>',
+		name: 'Navin',
+		age: '25',
+		assignmentLink:
+			'https://images.unsplash.com/photo-1576792254006-4f8b51ed204b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
 	},
 	methods: {
 		changeTitle: function(event) {
 			this.title = event.target.value;
+		},
+		mathRandom: function() {
+			return Math.random();
 		}
 	}
 });
