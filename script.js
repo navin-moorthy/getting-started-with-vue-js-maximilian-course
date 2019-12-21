f66eb69f668075723eb66dc861b81db40bd5ea70
Navin <navin007.a@gmail.com>
2019-12-21 09:00:10 +0530

    Using VueJS to Interact with the DOM

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ new Vue({
 		name: 'Navin',
 		age: '25',
 		assignmentLink:
-			'https://images.unsplash.com/photo-1576792254006-4f8b51ed204b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80'
+			'https://images.unsplash.com/photo-1576792254006-4f8b51ed204b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
+		counter: 0,
+		x: 0,
+		y: 0
 	},
 	methods: {
 		changeTitle: function(event) {
@@ -15,6 +18,17 @@ new Vue({
 		},
 		mathRandom: function() {
 			return Math.random();
+		},
+		increase: function(step, event) {
+			this.counter++;
+			console.log('Event:', event);
+		},
+		updateCoordinate: function(event) {
+			this.x = event.clientX;
+			this.y = event.clientY;
+		},
+		alertMe: function() {
+			alert('Exact Keys pressed!');
 		}
 	}
 });
