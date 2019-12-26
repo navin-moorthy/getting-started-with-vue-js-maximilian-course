c9ea0fab71f47fce7faf5ae0145412577538ee27
Navin <navin007.a@gmail.com>
2019-12-26 07:05:09 +0530

    Using VueJS to Interact with the DOM

--- script.js
+++ script.js
@@ -26,7 +26,19 @@ new Vue({
 			width: '0px',
 			backgroundColor: 'magenta'
 		},
-		isVisible: true
+		isVisible: true,
+		toggle: false,
+		array: ['Max', 'Anna', 'Chris', 'Manu'],
+		myObject: {
+			title: 'Lord of the Rings',
+			author: 'J.R.R. Tolkiens',
+			books: '3'
+		},
+		testData: {
+			name: 'TESTOBJECT',
+			id: 10,
+			data: [1.67, 1.33, 0.98, 2.21]
+		}
 	},
 	watch: {
 		result: function(value) {
